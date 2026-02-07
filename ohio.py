3d462e5d9d112ab07d6b99f87a1b2f072532fb68
Shaurya21032014 <haritika.arora@gmail.com>
2026-02-07 15:38:19 +0530

    Add files via upload

---
+++ ohio.py
@@ -0,0 +1,36 @@
+def print2odd(arr, size):
+    xorof2 = arr[0]
+
+
+    x = 0
+    y = 0
+
+    setbit = 0
+
+
+    for i in range(1, size):
+        xorof2 = xorof2 ^ arr[1]
+
+    setbit = xorof2 & ~(xorof2 - 1)
+
+
+    for i in range (size):
+        if(arr[i] & setbit):
+            x = x ^ arr[i]
+        else:
+            y= y ^ arr[i]
+
+    print("the 2 odd element are", x, "%", y )
+
+
+
+arr = []
+
+
+arr_size = int(input("enter a number pf array"))
+
+for i in range(0,arr_size):
+    z = int(input("enter element"))
+    arr.append(z)
+
+print2odd(arr, arr_size)
